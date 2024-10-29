c4dbe53e55789bad6bd0a40f0d4d56523813b8a6
CecilleHobbs <s203542@dtu.dk>
2024-10-29 11:01:26 +0100

    s203542 has made the package

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Title: CentralDogma
+#'
+#' This package has the purpose of transforming nucleotide sequence into one letter protein code
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_table"
